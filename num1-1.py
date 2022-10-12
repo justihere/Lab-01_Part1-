adbd78e5a9c876806181bc6cb4718bb5cababdd1
justihere <107419759+justihere@users.noreply.github.com>
2022-10-12 17:18:00 +0200

    Update num1-1.py

--- num1-1.py
+++ num1-1.py
@@ -1,6 +1,22 @@
-import argparse
+import argparse 
 
-if __name__ == "__main__" :
+def process(val_1, op, val_2):
+    if op == "+":
+        return val_1 + val_2
+    elif op == "-":
+        return val_1 - val_2
+    elif op == "*":
+        return val_1 * val_2
+    elif op == "/":
+        if val_2 == 0:
+            print("can't divide by zero")
+        return val_1 / val_2
+            
+    else:
+        print("Invalid operator")
+        exit()
+
+if name == "__main__" :
     parser = argparse.ArgumentParser(description="Take two integer values - ")
     parser.add_argument("input_value_1", type=int,
                 help="Give a positive integer value for 1st veriable")
@@ -14,16 +30,8 @@ if __name__ == "__main__" :
     val_2 = input_args.input_value_2
     operator = str(input_args.input_operator)
 
-    if operator == "+":
-        result = val_1 + val_2
-    elif operator == "-":
-        result = val_1 - val_2
-    elif operator == "*":
-        result = val_1 * val_2
-    elif operator == "/":
-        result = val_1 / val_2
 
-    print("{0} {1} {2} = {3}".format(val_1, operator, val_2, result))
+    print("{0} {1} = {2}".format(process(val_1, operator, val_2)))
 
 
 
